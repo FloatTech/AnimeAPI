483d6e3c98e857f3c9a5b885f51e5d8866df21c7
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-06-14 16:08:14 +0800

    ✨ 增加 saucenao 搜图

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/FloatTech/AnimeAPI/pixiv"
+	"github.com/FloatTech/AnimeAPI/saucenao"
 )
 
 func main() {
-	temp, _ := pixiv.Works(90281866)
-	fmt.Println(string(temp))
+	// pixiv
+	// temp, _ := pixiv.Works(90281866)
+	// fmt.Println(string(temp))
+	// saucenao
+	sau, err := saucenao.SauceNAO("http://gchat.qpic.cn/gchatpic_new/213864964/780718903-2150696581-2D6393542C1E07DA915BFEF89ECA0BBD/0?term=2")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(*sau)
+	}
 }

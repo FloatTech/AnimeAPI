1019c9e2b26f518cd78515b6fb4cdd5013332381
yuuki <tdf1939@hotmail.com>
2021-06-16 09:34:25 +0800

    main.go

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/FloatTech/AnimeAPI/pixiv"
 	"github.com/FloatTech/AnimeAPI/saucenao"
 )
 
 func main() {
-	// pixiv
-	// temp, _ := pixiv.Works(90281866)
-	// fmt.Println(string(temp))
-	// saucenao
+	id := "75841587"
+	data, err := pixiv.Works(id)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v\n", data)
+
 	sau, err := saucenao.SauceNAO("http://gchat.qpic.cn/gchatpic_new/213864964/780718903-2150696581-2D6393542C1E07DA915BFEF89ECA0BBD/0?term=2")
 	if err != nil {
 		fmt.Println(err)

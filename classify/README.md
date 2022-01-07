a942fd264e231508535d38f33c6f2dce5beecd40
fumiama <s862105088@gmail.com>
2022-01-07 21:45:48 +0800

    🔥 🎨  精简 classify

--- classify/README.md
+++ classify/README.md
@@ -2,23 +2,11 @@
 Zerobot-ACGImage插件的AI评分接口，也可单独引用。
 
 # 接口说明
-
-## func Init(dataPath string)
-设置数据缓存路径
-
-## func Flush()
-手动刷新图片缓存及访问时间戳。
-
-## func CanVisit(delay int64) bool
-距上次返回`true`时间间隔大于`delay`秒则返回`true`并刷新时间戳，用以避免频繁访问。`Classify`函数并不会做验证，因此请务必在调用`Classify`前手动验证，以避免由于缓存时间戳不正确导致的无法加载图片。
-
-请确保`delay`至少大于1，否则可能导致无法加载图片。
-
-## func Classify(targeturl string, noimg bool) (int, int64, string, string)
-用AI对`targeturl`指向的图片打分。返回值：class lastvisit dhash comment。
+## func Classify(targetURL string, isNoNeedImg bool) (int, string, string, []byte)
+用AI对`targeturl`指向的图片打分。返回值：class dhash comment data。
 
 - 如果`noimg==true`，将不下载图片。
-- 如果`noimg==false`，将下载图片到`dataPath/cache`+`lastvisit`。
+- 如果`noimg==false`，将下载图片到 data。
 - `dhash`为图片的dhash值的[base16384](https://github.com/fumiama/base16384)编码。
 - `comment`为针对该`class`的评语，详见下方打分等级。
 
--- classify/classify.go
+++ classify/classify.go
@@ -4,9 +4,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
@@ -15,10 +13,7 @@ import (
 const head = "https://sayuri.fumiama.top/dice?class=9&url="
 
 var (
-	datapath  string
-	cachefile string
-	lastvisit = time.Now().Unix()
-	comments  = []string{
+	comments = []string{
 		"[0]这啥啊",
 		"[1]普通欸",
 		"[2]有点可爱",
@@ -31,69 +26,43 @@ var (
 	}
 )
 
-// Init 设置 datapath
-func Init(dataPath string) {
-	datapath = dataPath
-	os.RemoveAll(datapath) // 清除缓存
-	err := os.MkdirAll(datapath, 0755)
-	if err != nil {
-		panic(err)
+// Classify 图片打分 返回值：class dhash comment, data
+func Classify(targetURL string, isNoNeedImg bool) (int, string, string, []byte) {
+	if targetURL[0] != '&' {
+		targetURL = url.QueryEscape(targetURL)
 	}
-	cachefile = datapath + "cache"
-}
 
-// Flush 刷新时间戳
-func Flush() {
-	lastvisit = time.Now().Unix()
-}
+	u := head + targetURL
+	if isNoNeedImg {
+		u += "&noimg=true"
+	}
+	resp, err := http.Get(u)
 
-// Canvisit 可以访问
-func CanVisit(delay int64) bool {
-	if time.Now().Unix()-lastvisit > delay {
-		Flush()
-		return true
-	}
-	return false
-}
-
-// Classify 图片打分 返回值：class lastvisit dhash comment
-func Classify(targeturl string, noimg bool) (int, int64, string, string) {
-	lv := lastvisit
-	if targeturl[0] != '&' {
-		targeturl = url.QueryEscape(targeturl)
-	}
-	get_url := head + targeturl
-	if noimg {
-		get_url += "&noimg=true"
-	}
-	resp, err := http.Get(get_url)
 	if err != nil {
 		log.Warnf("[AI打分] %v", err)
-		return 0, 0, "", ""
-	} else {
-		if noimg {
-			data, err1 := ioutil.ReadAll(resp.Body)
-			if err1 == nil {
-				dhash := gjson.GetBytes(data, "img").String()
-				class := int(gjson.GetBytes(data, "class").Int())
-				return class, lv, dhash, comments[class]
-			} else {
-				log.Warnf("[AI打分] %v", err1)
-				return 0, 0, "", ""
-			}
-		} else {
-			class, err1 := strconv.Atoi(resp.Header.Get("Class"))
-			dhash := resp.Header.Get("DHash")
-			if err1 != nil {
-				log.Warnf("[AI打分] %v", err1)
-			}
-			defer resp.Body.Close()
-			// 写入文件
-			data, _ := ioutil.ReadAll(resp.Body)
-			f, _ := os.OpenFile(cachefile+strconv.FormatInt(lv, 10), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
-			defer f.Close()
-			f.Write(data)
-			return class, lv, dhash, comments[class]
-		}
+		return 0, "", "", nil
 	}
+
+	if isNoNeedImg {
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Warnf("[AI打分] %v", err)
+			return 0, "", "", nil
+		}
+		dhash := gjson.GetBytes(data, "img").String()
+		class := int(gjson.GetBytes(data, "class").Int())
+		return class, dhash, comments[class], nil
+	}
+
+	class, err := strconv.Atoi(resp.Header.Get("Class"))
+	dhash := resp.Header.Get("DHash")
+	if err != nil {
+		log.Warnf("[AI打分] %v", err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Warnf("[AI打分] %v", err)
+	}
+	return class, dhash, comments[class], data
 }

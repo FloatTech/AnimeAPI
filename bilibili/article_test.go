9b6fd8c9f83d13a037c10db23e396f6c9c088162
guohuiyuan <1156544355@qq.com>
2022-07-30 16:11:36 +0800

    🔥 不用bilibili

--- bilibili/article_test.go
+++
@@ -1,7 +0,0 @@
-package bilibili
-
-import "testing"
-
-func TestArticleInfo(t *testing.T) {
-	t.Log(ArticleInfo("17279244"))
-}
--- bilibili/live_test.go
+++
@@ -1,7 +0,0 @@
-package bilibili
-
-import "testing"
-
-func TestLiveRoomInfo(t *testing.T) {
-	t.Log(LiveRoomInfo("83171"))
-}

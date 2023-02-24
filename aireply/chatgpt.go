bfc126d123e9a5c02c4cbe66457fb7ccda903db0
莫思敛 <55676105+shudorcl@users.noreply.github.com>
2023-02-24 22:37:26 +0800

    optimize: chat

--- aireply/chatgpt.go
+++ aireply/chatgpt.go
@@ -53,10 +53,7 @@ func (*ChatGPT) String() string {
 
 // Talk 取得带 CQ 码的回复消息
 func (c *ChatGPT) Talk(_ int64, msg, nickname string) string {
-	replystr, err := chat(msg, c.k, c.u)
-	if err != nil {
-		return err.Error()
-	}
+	replystr := chat(msg, c.k, c.u)
 	for _, w := range c.b {
 		if strings.Contains(replystr, w) {
 			return "ERROR: 回复可能含有敏感内容"
@@ -70,7 +67,7 @@ func (c *ChatGPT) TalkPlain(_ int64, msg, nickname string) string {
 	return c.Talk(0, msg, nickname)
 }
 
-func chat(msg string, apiKey string, url string) (string, error) {
+func chat(msg string, apiKey string, url string) string {
 	requestBody := chatGPTRequestBody{
 		Model:            "text-davinci-003",
 		Prompt:           msg,
@@ -80,14 +77,14 @@ func chat(msg string, apiKey string, url string) (string, error) {
 		FrequencyPenalty: 0,
 		PresencePenalty:  0,
 	}
-	requestData := new(bytes.Buffer)
-	err := json.NewEncoder(requestData).Encode(requestBody)
+	requestData := bytes.NewBuffer(make([]byte, 0, 1024*1024))
+	err := json.NewEncoder(requestData).Encode(&requestBody)
 	if err != nil {
-		return "", err
+		return err.Error()
 	}
 	req, err := http.NewRequest("POST", url+"completions", requestData)
 	if err != nil {
-		return "", err
+		return err.Error()
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -95,20 +92,18 @@ func chat(msg string, apiKey string, url string) (string, error) {
 	client := &http.Client{}
 	response, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return err.Error()
 	}
 	defer response.Body.Close()
 	var gptResponseBody chatGPTResponseBody
 	err = json.NewDecoder(response.Body).Decode(&gptResponseBody)
 	if err != nil {
-		return "", err
+		return err.Error()
 	}
-	var reply string
 	if len(gptResponseBody.Choices) > 0 {
 		for _, v := range gptResponseBody.Choices {
-			reply = fmt.Sprint(v["text"])
-			break
+			return fmt.Sprint(v["text"])
 		}
 	}
-	return reply, nil
+	return ""
 }

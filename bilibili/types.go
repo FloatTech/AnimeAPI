50f5c7587c5bf6d833ab7483d054c226feb74f1b
vatebur <67186678+vatebur@users.noreply.github.com>
2025-05-30 13:50:06 +0800

    Rollback bilibili API (#72)

--- bilibili/types.go
+++ bilibili/types.go
@@ -53,8 +53,6 @@ const (
 	OnlineTotalURL = "https://api.bilibili.com/x/player/online/total?bvid=%v&cid=%v"
 	// NavURL 导航URL
 	NavURL = "https://api.bilibili.com/x/web-interface/nav"
-	// ConstUA 浏览器UA
-	ConstUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
 )
 
 // DynamicCard 总动态结构体,包括desc,card

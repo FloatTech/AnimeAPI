ed4ff6e0660874844de5702f0f7c50ad2a525eb7
小宇宇 <3061196825@qq.com>
2025-08-14 23:52:37 +0800

    删除hooksMtx锁

--- niu/db.go
+++ niu/db.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"github.com/RomiChan/syncx"
 	"github.com/jinzhu/gorm"
-	"sync"
 )
 
 var (
 	migratedGroups = syncx.Map[string, bool]{} // key: string, value: bool
 	tableHooks     []tableHook
-	hooksMtx       sync.RWMutex
 )
 
 type tableHook func(gid int64) error
@@ -44,21 +42,17 @@ func ensureAuctionInfo(gid int64) error {
 
 // registerTableHook 注册钩子
 func registerTableHook(h ...tableHook) {
-	hooksMtx.Lock()
-	defer hooksMtx.Unlock()
 	tableHooks = append(tableHooks, h...)
 }
 
 // TableFor 大写是为了防止数据操作哪里有问题留个保底可以在zbp的项目里直接改
 func TableFor(gid int64, prefix string) *Model {
-	// 先执行钩子
-	hooksMtx.RLock()
+	// 先跑钩子
 	for _, h := range tableHooks {
 		if err := h(gid); err != nil {
-			panic(fmt.Sprintf("执行表钩子失败: %v", err))
+			panic(fmt.Sprintf("执行钩子失败: %v", err))
 		}
 	}
-	hooksMtx.RUnlock()
 
 	tableName := fmt.Sprintf("group_%d_%s_info", gid, prefix)
 	return &Model{db.Table(tableName)}
